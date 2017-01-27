1139806beaee69450e69c00f529ec2b232933810
nzoLogic <nzoLogic@gmail.com>
2017-01-27 15:40:02 -0800

    user log in and password hashing is working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,18 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      redirect_to new_user
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'users#index'
 
   #on this route use the 'UsersController' with new action with the hash as new_user
-  get '/users/new', to: 'users#new', as: 'new_user'
+  get '/users/new' => 'users#new'
+  post '/users' => 'users#create'
   resources :users
 end
